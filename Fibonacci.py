3acf142f06c7069ef5669c3fa34ae70456751f78
JakeJazokas <jakejazokas@outlook.com>
2021-11-09 13:59:55 -0500

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,19 @@
+'''
+Your goal is to create a function that will output any given Fibonacci number (num)
+
+For example, fib(1) should return 1 (1+0) and fib(3) should return 3 (1+2)
+
+Good Luck!
+'''
+
+def fib(num):
+  #Your Code Goes Here
+
+if __name__ == "__main__":
+    #Code to test your function
+    print("The 1st Fibonacci Number:")
+    print(fib(1) == 1)
+    print("The 5th Fibonacci Number:")
+    print(fib(5) == 5)
+    print("The 10th Fibonacci Number:")
+    print(fib(10) == 55)
